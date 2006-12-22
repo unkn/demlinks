086aa2c2c84cb89dcf631633381909d9cc5f38ed
AtKaaZ <atkaaz@users.sourceforge.net>
2006-12-22 06:51:15 +0000

    * added GNU GPL license reminder, if not already known that it is under GPL; or wtw

--- php_pdo_base/dmlL0def.php
+++ php_pdo_base/dmlL0def.php
@@ -2,6 +2,36 @@
 #ifndef DMLL0DEF_PHP
 #define DMLL0DEF_PHP
 
+/****************************************************************************
+*
+*                             dmental links
+*    Copyright (C) 2006 AtKaaZ, AtKaaZ at users.sourceforge.net
+*
+*  ========================================================================
+*
+*    This program is free software; you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation; either version 2 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*  ========================================================================
+*
+* Description: demlinks level 0 defines, also used by level 1
+*
+****************************************************************************/
+
+
+
+
 define(dbasename,"demlinks6.3sql");
 define(dNodeNames,"NodeNames");//table name
 define(dRelations,"Relations");//table name
--- php_pdo_base/shortdef.php
+++ php_pdo_base/shortdef.php
@@ -2,6 +2,36 @@
 #ifndef SHORTDEF_PHP
 #define SHORTDEF_PHP
 
+/****************************************************************************
+*
+*                             dmental links
+*    Copyright (C) 2006 AtKaaZ, AtKaaZ at users.sourceforge.net
+*
+*  ========================================================================
+*
+*    This program is free software; you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation; either version 2 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program; if not, write to the Free Software
+*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*  ========================================================================
+*
+* Description: common defines to be used in php sources
+*
+****************************************************************************/
+
+
+
+
 #include "color.php"
 
         define(nl,"\n");
@@ -12,8 +42,6 @@
         define(no,redcol."no".nocol);
         define(ok,yes);//these must be equivalent:  ok~yes and bad~no   where yes="yes" and no="no" or wtw
         define(bad,no);//certain operations interchange ok with yes, that's why!
-
-//#define many(...) array(__VA_ARGS__)
 
 #endif //header
 //?>
