723344823d743cfafc888fe65c14a8405f520368
AtKaaZ <atkaaz@users.sourceforge.net>
2007-01-25 05:07:09 +0000

    * unkn's css for cmenu , in ff * rename ID when children aren't seen, idea

--- php_pdo_base/cmenu.js
+++ php_pdo_base/cmenu.js
@@ -9,14 +9,16 @@ if (ie5||ns6)
 else
 	alert("The ContextMenu only works in IE5+ or NS6/Firefox");
 
-function newitem(text, action, id)
+function newitem(text, urlaction)
 {
         var d1=document.createElement('div');
 
-        if (""!=action) {
+        if (""!=urlaction) {
                 //d1.setAttribute('class','menuitems');
                 d1.className='menuitems';
-                d1.setAttribute('url','dothis.php?action='+action+'&onwhat='+escape(id));
+                //d1.setAttribute('url','dothis.php?action='+action+'&onwhat='+escape(id));
+                d1.setAttribute('url',urlaction); //'javascript:var something=prompt("Find what ID:",something);');
+                d1.style.cursor='pointer';
         } else {
                 //d1.setAttribute('class','nonmenuitems');
                 d1.className='nonmenuitems';
@@ -25,6 +27,21 @@ function newitem(text, action, id)
         //d1.innerHTML=text;
         menuobj.appendChild(d1);
 }
+
+var renamewith="";
+function rename(what)
+{
+        //renamewith=prompt("newname:",renamewith);
+        var parent1=what.parentNode;
+        var newinput=document.createElement('input');
+        newinput.value=unescape(what.id);
+        newinput.className="renamebox";
+        var oldone=parent1.replaceChild(newinput, what);
+        alert(what.id);
+        parent1.replaceChild(oldone,newinput);
+}
+
+var passer;
 function showmenuie5(e){
 //Find out how close the mouse is to the corner of the window
 	var onwhat=ie5? event.srcElement : e.target;
@@ -39,9 +56,12 @@ function showmenuie5(e){
 
         menuobj.innerHTML="";
 
-        if (onwhatclass=="node") {
-                newitem(onwhatid,"",onwhatid);
-                newitem('Add new child...',"add",onwhatid);
+        if (onwhatclass=="node" || onwhatclass=="root"||onwhatclass=="leaf") {
+                newitem(onwhatid,"");
+                newitem('Add New Child...',"javascript:var addchild=prompt(\"Add New Child ID to "+onwhatid+":\",addchild);");
+                newitem('Find ID...',"javascript:var findthis=prompt(\"Find what ID:\", findthis);");
+                passer=onwhat;
+                newitem('Rename ID...',"javascript:rename(passer);");
                 //newitem('<a href="javascript:ddtreemenu.flatten(\''+eemenuid.'\', \'expand\')">Expand All</a> | <a href="javascript:ddtreemenu.flatten(\''.$treemenuid.'\', \'contract\')">Contract All</a>'.rnl;
 
         } else {
@@ -74,9 +94,9 @@ function showmenuie5(e){
 function semihidemenu(e){
 	var firingobj=ie5? event.srcElement : e.target
         try {
-        if (firingobj.className.indexOf('menuitems') == -1)
-	        menuobj.style.visibility="hidden"
-} catch(m) {}
+                if (firingobj.className.indexOf('menuitems') == -1)
+                menuobj.style.visibility="hidden"
+        } catch(m) {}
         //dumbvar=false;
 }
 
@@ -84,8 +104,8 @@ function highlightie5(e){
 	var firingobj=ie5? event.srcElement : e.target
 	if (firingobj.className=="menuitems"||ns6&&firingobj.parentNode.className=="menuitems"){
 		if (ns6&&firingobj.parentNode.className=="menuitems") firingobj=firingobj.parentNode //up one node
-		firingobj.style.backgroundColor="highlight"
-		firingobj.style.color="white"
+		//firingobj.style.backgroundColor="highlight"
+		//firingobj.style.color="white"
 		if (display_url==1) {//only works in IE && ff, both need to allow status to be changed
 			window.status=(ie5? event.srcElement.url:e.target.getAttribute('url'));
                 }
@@ -96,8 +116,8 @@ function lowlightie5(e){
 	var firingobj=ie5? event.srcElement : e.target
 	if (firingobj.className=="menuitems"||ns6&&firingobj.parentNode.className=="menuitems"){
 		if (ns6&&firingobj.parentNode.className=="menuitems") firingobj=firingobj.parentNode //up one node
-		firingobj.style.backgroundColor=""
-		firingobj.style.color="black"
+		//firingobj.style.backgroundColor=""
+		//firingobj.style.color="black"
 		window.status=''
 	}
 }
@@ -106,10 +126,11 @@ function jumptoie5(e){
 	var firingobj=ie5? event.srcElement : e.target
 	if (firingobj.className=="menuitems"||ns6&&firingobj.parentNode.className=="menuitems"){
 		if (ns6&&firingobj.parentNode.className=="menuitems") firingobj=firingobj.parentNode
-		/*if (firingobj.getAttribute("target"))
+                //alert('matah');
+		if (firingobj.getAttribute("target"))
 		        window.open(firingobj.getAttribute("url"),firingobj.getAttribute("target"))
 	        else
-		        window.location=firingobj.getAttribute("url")*/
+		        window.location=firingobj.getAttribute("url")
 	}
 }
 
