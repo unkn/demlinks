e11de6808ba88b7ec55461fe20e75456322d34b3
AtKaaZ <atkaaz@users.sourceforge.net>
2007-01-25 06:10:14 +0000

    * prompt for renameID

--- php_pdo_base/cmenu.js
+++ php_pdo_base/cmenu.js
@@ -28,17 +28,29 @@ function newitem(text, urlaction)
         menuobj.appendChild(d1);
 }
 
-var renamewith="";
-function rename(what)
+var lastnewnode="";
+function renameprompt(what)
 {
-        //renamewith=prompt("newname:",renamewith);
-        var parent1=what.parentNode;
-        var newinput=document.createElement('input');
-        newinput.value=unescape(what.id);
-        newinput.className="renamebox";
-        var oldone=parent1.replaceChild(newinput, what);
-        alert(what.id);
-        parent1.replaceChild(oldone,newinput);
+        //var parent1=what.parentNode;
+        //var newinput=document.createElement('input');
+        //newinput.className="renamebox";
+        var id=unescape(what.id);
+        var old=what.style.color;
+        what.style.color="#FF0000";
+        //newinput.value=id;
+        //var oldone=parent1.replaceChild(newinput, what);
+        var newid=prompt(id+"\nnewname:",id);
+        if (newid!=null && newid != id) {//ok pressed
+                ajaxrename(what,newid);
+        }
+        what.style.color=old;
+        //alert(what.id);
+        //parent1.replaceChild(oldone,newinput);
+}
+
+function ajaxrename(old, newname)
+{
+        alert("ajaxrename");
 }
 
 var passer;
@@ -50,7 +62,7 @@ function showmenuie5(e){
         var onwhatclass=onwhat.className;
 
         try { //rightclicked on an already open contexmenu? do nothing
-                if (onwhatclass.indexOf('menuitems') != -1)
+                if (onwhatclass.indexOf('menuitems') != -1 )
                         return false;
         } catch(m) {}
 
@@ -61,7 +73,7 @@ function showmenuie5(e){
                 newitem('Add New Child...',"javascript:var addchild=prompt(\"Add New Child ID to "+onwhatid+":\",addchild);");
                 newitem('Find ID...',"javascript:var findthis=prompt(\"Find what ID:\", findthis);");
                 passer=onwhat;
-                newitem('Rename ID...',"javascript:rename(passer);");
+                newitem('Rename ID...',"javascript:renameprompt(passer);");
                 //newitem('<a href="javascript:ddtreemenu.flatten(\''+eemenuid.'\', \'expand\')">Expand All</a> | <a href="javascript:ddtreemenu.flatten(\''.$treemenuid.'\', \'contract\')">Contract All</a>'.rnl;
 
         } else {
@@ -94,8 +106,8 @@ function showmenuie5(e){
 function semihidemenu(e){
 	var firingobj=ie5? event.srcElement : e.target
         try {
-                if (firingobj.className.indexOf('menuitems') == -1)
-                menuobj.style.visibility="hidden"
+                if (firingobj.className != 'menuitems' )
+                        menuobj.style.visibility="hidden"
         } catch(m) {}
         //dumbvar=false;
 }
