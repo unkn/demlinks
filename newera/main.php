17c769f92eb5c543a94a1de1423261dbd2e81a5c
AtKaaZ <atkaaz@users.sourceforge.net>
2007-04-12 21:23:30 +0000

    * same as the old one, but simpler

--- newera/main.php
+++ newera/main.php
@@ -11,6 +11,68 @@ require_once("dmlDBL1.php");
         } else {
                 report("...using prev. defined table");
         }
+        $res=split("[ .,/\\\"\?\<\>&!;|\#\$\*\+\{\}=\(\)'`\n\-]",file_get_contents("dmlDBL0def.php"));
+        exceptif(1===count($res));
+        exceptifnot( $dmlDB->OpenTransaction() );
+        $aborted=false;
+        $cnt=0;
+        foreach ($res as $val) {
+                     $val=trim($val);
+                if (!empty($val)) {//ie. non-empty
+
+                   try {
+
+                        exceptifnot( $ret=$dmlDB->AddName($val) );
+
+                        if (in_array(kAlready,$ret)) {
+                                echo redcol;
+                        } else {
+                                echo greencol;
+                        }
+                        echo $val.nocol." ";//.nl;
+                        //usleep(100000);
+                        $cnt++;//echo "cnt=".$cnt.nl;
+
+                        //if ($cnt % 15 == 0) {
+                                //_yntIFnot( $dmlDB->CloseTransaction() );
+                        //}
+
+                   }
+                   catch(PDOException $e) {
+                                exceptifnot( $dmlDB->AbortTransaction());
+                                $aborted=true;
+                                break;
+                   }
+                   catch(Exception $e) {
+                                exceptifnot( $dmlDB->AbortTransaction());
+                                $aborted=true;
+                                break;
+                   }
+                } //fi
+        }//foreach
+
+        echo nocol.nl;
+        if (!$aborted) {
+                exceptifnot( $dmlDB->CloseTransaction() );
+        }
+        echo nocol.nl;
+
+        exceptifnot( $dmlDB->IsName("if") );
+
+        exceptifnot( $dmlDB->Show($into) );
+        $arr=$into->fetchAll();
+        $count=count($arr);
+        report( "Before del: $count times.");
+
+        exceptifnot($dmlDB->DelName("if") );
+        $dmlDB->IsName("if");
+
+        exceptifnot( $dmlDB->Show($into) );
+        $arr=$into->fetchAll();
+        $count=count($arr);
+        report( "After del:  $count times.");
+
+        $dmlDB=null;//ie. dispose()
 
 
 ?>
