94b0f1ebb3a14efd01266d25caa39fca6e24e060
AtKaaZ <atkaaz@users.sourceforge.net>
2007-04-14 19:57:06 +0000

    * simpler

--- newera/rollBacktest.php
+++ newera/rollBacktest.php
@@ -1,83 +1,34 @@
 <?php
+//to test this program u must run it twice at the same time in ie. two terminals
+//this program suposedly creates a new dbase with table name NodeNames and one field in it named NodeName
+//it then begins a transaction then attempts to read an element 'zTest' of field 'NodeName' which obv. doesn't exist, ignoring the returned errors
+//then it writes it(since it wasn't there)
+//then decides to rollBack the transaction and eventually try a new one
+//because rollBack doesn't really work(apparently) for some unknown reason, beginTransaction fails saying 'There is already an active transaction'
+$db = new PDO('sqlite:demlinks6.3sql',''/*user*/,''/*pwd*/);
 
- $db = new PDO('sqlite:demlinks6.3sql',''/*user*/,''/*pwd*/);
-
-$crea='CREATE TABLE \'NodeNames\' ("NodeName" VARCHAR(10));';
-//$crea2='CREATE TABLE \'Relations\' ("ParentNodeID" INTEGER PRIMARY KEY , "ChildNodeID" INTEGER SECONDARY KEY);';
-
- $db->exec($crea);
-// $db->exec($crea2);
+$db->exec('CREATE TABLE \'NodeNames\' ("NodeName" VARCHAR(10));');
 
 $db->beginTransaction();
 
-                $getter="zTest";
-                //--------- get Name by ID
-                $pgn = $db->prepare('SELECT * FROM \'NodeNames\' WHERE "NodeName" = :node13');
-                $pgn->bindParam(":node13", $getter, PDO::PARAM_STR);
-        //read
-        $pgn->execute();//execute above SELECT
-        $ar=$pgn->FetchAll();//get array of results
-
-
-/*function read($now) {
-        //global $getter;
-        //$getter=$now;
-        global $pgn;
-        $pgn->execute();//execute above SELECT
-        $ar=$pgn->FetchAll();//get array of results
-        //print_r($ar);
-        if (empty($ar)) {
-                return false;
-        }
-        return true;
-}*/
-//read("zTest");
-//it prolly doesn't exist so let's add it:
+$getter="zTest";
+$pgn = $db->prepare('SELECT * FROM \'NodeNames\' WHERE "NodeName" = :node13');
+$pgn->bindParam(":node13", $getter, PDO::PARAM_STR);
+//read
+$pgn->execute();//execute above SELECT
+$ar=$pgn->FetchAll();//get array of results
 
 $writter="zTest";
 $pnn=$db->prepare('INSERT INTO \'NodeNames\' ("NodeName") VALUES (:node14)');
 $pnn->bindParam(":node14", $writter, PDO::PARAM_STR);
-        //write
-        $pnn->execute();//write it!
+//write
+$pnn->execute();//write it!
 
-/*function write($now) {
-        //global $writter;
-        //$writter=$now;
-        global $pnn;
-        $pnn->execute();//write it!
-}*/
-
-/*
-        $res=split("[ .,/\\\"\?\<\>&!;|\#\$\*\+\{\}=\(\)'`\n\-]",file_get_contents("dmlDBL0def.php"));
-$db->beginTransaction();
-        foreach ($res as $val) {
-                usleep(10000);
-                $val=trim($val);
-                if (!empty($val)) {//ie. non-empty
-                        if (false===read($val)) {
-                                echo greencol;
-                                write($val);
-                        } else {
-                                echo redcol;
-                        }
-                        echo $val." ";
-                }//if
-        }//foreach
-*/
-
-//echo read("zTest");
-//write("zTest");
-/*echo read("zTest");
-write("zTest");
-echo read("zTest");*/
 echo "waiting...";
 usleep(2000000);
-echo "\n";
-//echo read("zTest");
-//write("zTest");
-
- //$db->
- $db->rollBack();//this doesn't do it's job
- $db->beginTransaction();//here it fails, when running this program twice at the same time; 'There is already an active transaction'
-
+echo "done\n";
+$db->rollBack();//this doesn't do it's job
+$db->beginTransaction();//here it fails, when running this program twice at the same time; 'There is already an active transaction'
+//unreachable:
+$db->commit();
 ?>
