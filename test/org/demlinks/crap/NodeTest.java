98da64c043cb968b3cd8e6ddfd045051b6a10ec1
AtKaaZ <atkaaz@users.sourceforge.net>
2009-02-04 01:57:21 +0000

    test

--- test/org/demlinks/crap/NodeTest.java
+++ test/org/demlinks/crap/NodeTest.java
@@ -1,6 +1,23 @@
 package org.demlinks.crap;
 
+import org.junit.Before;
+import org.junit.Test;
 
 public class NodeTest {
 
+	Node a, b;
+
+	@Before
+	public void init() {
+		a = new Node();
+		b = new Node();
+	}
+
+	@Test
+	public void testAppendChild() {
+		assertTrue(a.appendChild(b));
+		assertTrue(a.hasChild(b));
+		assertFalse(a.appendChild(b));
+		assertTrue(a.hasChild(b));
+	}
 }
