850abff67cf7cb5373bafc35488a3206e08836f4
AtKaaZ <atkaaz@users.sourceforge.net>
2009-02-07 03:10:42 +0100

    done with NodeList tests, ListCursor is left

--- src/org/demlinks/crap/NodeList.java
+++ src/org/demlinks/crap/NodeList.java
@@ -1,5 +1,7 @@
 package org.demlinks.crap;
 
+import javax.naming.CannotProceedException;
+
 import org.demlinks.debug.Debug;
 
 public class NodeList {
@@ -16,7 +18,12 @@ public class NodeList {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
+	
+	public Node getFirstNode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	public Node getLastNode() {
 		// TODO Auto-generated method stub
 		return null;
@@ -30,5 +37,72 @@ public class NodeList {
 	public boolean isEmpty() {
 		return this.size() == 0;
 	}
+
+	public Node getNodeAfter(Node node) {
+		Debug.nullException(node);
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Node getNodeBefore(Node node) {
+		Debug.nullException(node);
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean hasNode(Node node) {
+		Debug.nullException(node);
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * @param whichNode
+	 * @param whatPos
+	 * @return true if whichNode existed before, and it's now still there, not moved<br>
+	 * 			false if, whichNode didn't exist and it's now exactly where specified
+	 * 			by call
+	 */
+	public boolean insertNode(Node whichNode, Position whatPos) {
+		Debug.nullException(whichNode, whatPos);
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean insertAfterNode(Node newNode, Node afterNode) {
+		Debug.nullException(newNode, afterNode);
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * @param node2
+	 * @return true if node existed before call, not anymore after the call
+	 * 			false if failed to delete because it didn't exist
+	 * @throws CannotProceedException if node still exists after call, couldn't delete it
+	 */
+	public boolean removeNode(Node node) throws CannotProceedException {
+		Debug.nullException(node);
+		throw new CannotProceedException();
+		//TODO make own exception here
+		// TODO Auto-generated method stub
+		//return false;
+	}
+
+	/**
+	 * @param newNode
+	 * @param beforeNode
+	 * @return true if newNode existed before call, and it's still there
+	 * 			in the same place as before, 
+	 * 			hence it wasn't moved before beforeNode<br>
+	 * 		false is newNode didn't exist, but it does now and it's right before
+	 * 		beforeNode
+	 */
+	public boolean insertBeforeNode(Node newNode, Node beforeNode) {
+		Debug.nullException(newNode, beforeNode);
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 
 }
--- test/org/demlinks/crap/NodeListTest.java
+++ test/org/demlinks/crap/NodeListTest.java
@@ -2,19 +2,22 @@ package org.demlinks.crap;
 
 import static org.junit.Assert.*;
 
+import javax.naming.CannotProceedException;
+
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class NodeListTest {
 
-	Node node1, node2, nullNode;
+	Node node1, node2, node3, nullNode;
 	NodeList list;
 	
 	@Before
 	public void init() {
 		node1 = new Node();
 		node2 = new Node();
+		node3 = new Node();
 		nullNode = null;
 		list = new NodeList();
 	}
@@ -22,6 +25,11 @@ public class NodeListTest {
 	@Test
 	public void testAppendNode() {
 		assertTrue(list.isEmpty());
+		assertTrue(list.getFirstNode() == null);
+		assertTrue(list.getLastNode() == null);
+		assertFalse(list.hasNode(node1));
+		assertFalse(list.hasNode(node2));
+		
 		assertFalse(list.appendNode(node1)); //false= node didn't already exist
 		assertTrue(node1 == list.getLastNode());
 		assertFalse(list.appendNode(node2));
@@ -37,5 +45,136 @@ public class NodeListTest {
 			excepted = true;
 		}
 		assertTrue(excepted);
+		
+		assertTrue( node2 == list.getNodeAfter(node1) );
+		assertTrue( node1 == list.getNodeBefore(node2) );
+		assertTrue(list.getNodeAfter(node2) == null);
+		assertTrue(list.getNodeBefore(node1) == null);
+		
+		excepted = false;
+		try {
+			list.hasNode(nullNode);
+		}catch (NullPointerException e) {
+			excepted = true;
+		}
+		assertTrue(excepted);
+		
+		assertTrue(list.hasNode(node1));
+		assertTrue(list.hasNode(node2));
 	}
+
+	@Test
+	public void testInsertNode() {
+		assertTrue(list.isEmpty());
+		assertFalse(list.insertNode(node1, Position.FIRST));
+		assertFalse(list.insertNode(node2, Position.LAST));
+		assertTrue(list.getFirstNode() == node1);
+		assertTrue(list.getLastNode() == node2);
+		assertTrue(list.size() == 2);
+		
+		boolean excepted = false;
+		try {
+			list.insertNode(nullNode, Position.FIRST);
+		}catch (NullPointerException e) {
+			excepted = true;
+		}
+		assertTrue(excepted);
+		
+		excepted = false;
+		try {
+			list.insertNode(node1, null);
+		}catch (NullPointerException e) {
+			excepted = true;
+		}
+		assertTrue(excepted);
+		
+		excepted = false;
+		try {
+			list.insertNode(null, null);
+		}catch (NullPointerException e) {
+			excepted = true;
+		}
+		assertTrue(excepted);
+	}
+	
+
+	@Test
+	public void testInsertAfterNode() throws CannotProceedException {
+		assertTrue(list.isEmpty());
+		assertFalse(list.appendNode(node1));
+		assertFalse(list.insertAfterNode(node2, node1));
+		assertTrue(list.getLastNode() == node2);
+		assertFalse(list.insertAfterNode(node3, node2));
+		assertTrue(list.getLastNode() == node3);
+		assertTrue(list.size() == 3);
+		
+		assertTrue(list.removeNode(node2));
+		assertFalse(list.removeNode(node2));
+		assertTrue(list.size() == 2);
+		assertTrue(list.hasNode(node1));
+		assertTrue(list.hasNode(node3));
+		
+		assertFalse(list.insertBeforeNode(node2, node3));
+		assertTrue(list.getNodeAfter(node1) == list.getNodeBefore(node3));
+		assertTrue(list.getNodeAfter(node1) == node2);
+		
+		
+		
+		boolean excepted = false;
+		try {
+			list.insertAfterNode(nullNode, node2);
+		}catch (NullPointerException e) {
+			excepted = true;
+		}
+		assertTrue(excepted);
+		
+		excepted = false;
+		try {
+			list.insertAfterNode(node1, nullNode);
+		}catch (NullPointerException e) {
+			excepted = true;
+		}
+		assertTrue(excepted);
+		
+		excepted = false;
+		try {
+			list.insertAfterNode(nullNode, nullNode);
+		}catch (NullPointerException e) {
+			excepted = true;
+		}
+		assertTrue(excepted);
+		
+		excepted = false;
+		try {
+			list.insertBeforeNode(nullNode, node2);
+		}catch (NullPointerException e) {
+			excepted = true;
+		}
+		assertTrue(excepted);
+		
+		excepted = false;
+		try {
+			list.insertBeforeNode(node1, nullNode);
+		}catch (NullPointerException e) {
+			excepted = true;
+		}
+		assertTrue(excepted);
+		
+		excepted = false;
+		try {
+			list.insertBeforeNode(nullNode, nullNode);
+		}catch (NullPointerException e) {
+			excepted = true;
+		}
+		assertTrue(excepted);
+		
+		excepted = false;
+		try {
+			list.removeNode(nullNode);
+		}catch (NullPointerException e) {
+			excepted = true;
+		}
+		assertTrue(excepted);
+	}
+
 }
