e2ec21600c5040ecabcab757998234ca51ff8231
AtKaaZ <atkaaz@users.sourceforge.net>
2009-03-09 21:56:31 +0100

    that seems right, but not tested yet

--- src/org/demlinks/environment/WordMapping.java
+++ src/org/demlinks/environment/WordMapping.java
@@ -9,7 +9,6 @@ import org.demlinks.errors.BugError;
 import org.demlinks.exceptions.BadParameterException;
 import org.demlinks.node.Node;
 import org.demlinks.node.NodeList;
-import org.demlinks.nodemaps.CharNode;
 import org.demlinks.nodemaps.Environment;
 import org.demlinks.nodemaps.IntermediaryNode;
 import org.demlinks.nodemaps.NodeWithDupChildren;
@@ -61,7 +60,7 @@ public class WordMapping extends CharMapping {
 	 * @param word
 	 * @return
 	 */
-	public WordNode getNodeForWord( String word ) {
+	public NodeList getNodeForWord( String word ) {
 
 		char c;
 		Node n;
@@ -85,109 +84,186 @@ public class WordMapping extends CharMapping {
 			// parent.hasChildAtPos(n, i);
 			nl.appendNode( n );
 		}
-		//list of all WordNodes that match this word
+		// list of all WordNodes that match this word
 		NodeList solutions = new NodeList();
 		// we have them all chars in 'nl'
 		// and in effect we can parse them in any direction
 		// TODO
 		
-		int vertIndex = 0;
-		IntermediaryNode inFor0=null;//intermediary nodes for word[0]
+		IntermediaryNode intermediaryNodeForNodeOnPos0 = null;// intermediary
+		// nodes
+		// for word[0]
+		Node nodeThatHasToBeOnPos0 = nl.getNodeAt( 0 );// first char
 		
-		NEXTWORDNODE:
-		int charIndex = 0;
-		//attempts to find next WordNode for word[charIndex], that's a different parent
-		inFor0 = this.getNextIntermediaryNodeForCharAt(
-				(CharNode)nl.getNodeAt( charIndex ), charIndex, inFor0 );
-		if ( null == inFor0 ) {
-			// none found, hence there's no (more)word(s) having word[0] at index 0
-			return solutions;//can be empty
-		}
-		NodeWithDupChildren wordNode = inFor0.getFather();
-		
-		IntermediaryNode in = inFor0;
-		
-		NEXTCHAR:
-		charIndex++;
-		
-		if ( charIndex >= word.length() ) {
-			// done with the wordNode, try next wordNode
-			//wordNode is one solution, but there can be more
-			solutions.addLast( wordNode );
-			goto NEXTWORDNODE;//from word[0] again
-		} else {
-			//we're on the next char,
+		int indexOfNextExpectedChar = 1;// 0 based though
+		while ( indexOfNextExpectedChar <= ( word.length() - 1 ) ) {
+			// the while if, will help us handle 1 char words; nothing else
+			// ie. the while will be broken only if our word is 1 char long,
+			// hence index=0
 			
+			// attempts to find next WordNode for word[charIndex], that's a
+			// different parent
+			// like parallel on the Z axis; same child CharNode
+			intermediaryNodeForNodeOnPos0 = this.getNextIntermediaryNodeForNodeAt(
+					nodeThatHasToBeOnPos0, 0, intermediaryNodeForNodeOnPos0 );
+			if ( null == intermediaryNodeForNodeOnPos0 ) {
+				// none found, hence there's no (more)word(s) having word[0] at
+				// index 0
+				break;// this is the only one that will break the while
+			}
+			NodeWithDupChildren wordNode = intermediaryNodeForNodeOnPos0.getFather();
+			if ( null == wordNode ) {
+				throw new BugError( "intermediary node w/o father?!" );
+			}
 			
-			HERE:
-			in = wordNode.getNextIntermediary( in );//for the same wordNode
-			if ( null != in ) {
-				//found one, which could be CharNode or WordNode
-				Node whatIs = in.getPointee();
-				if (Environment.isCharNode( whatIs )) {
-					//found a char, must be equal to word[charIndex]
-					if (nl.getNodeAt( charIndex ) == whatIs) {
-						//all ok, go next char
-						//charIndex++;
-						goto NEXTCHAR;
+			// wordNode already has intermediaryNodeForNodeOnPos0 on pos 0, thus
+			// we try finding next chars of word from pos 1 in wordNode
+			// continuing from pos 1
+			int backup = indexOfNextExpectedChar;
+			indexOfNextExpectedChar = this.digDeep( wordNode, nl,
+					indexOfNextExpectedChar, intermediaryNodeForNodeOnPos0 );
+			
+			if ( indexOfNextExpectedChar < 0 ) {
+				// ie. -1 from encountering bad char
+				// bad wordNode, need to get next wordNode
+				indexOfNextExpectedChar = backup;
+				continue;
+			} else {
+				if ( indexOfNextExpectedChar < word.length() - 1 ) {
+					// there's still some char(s) left
+					// we would ideally go UP, and keep
+					// indexOfNextExpectedChar
+					// where it is
+					// TODO
+					nodeThatHasToBeOnPos0 = wordNode;
+					intermediaryNodeForNodeOnPos0 = null;// start from
+					// beginning
+					continue;
+				} else {
+					if ( indexOfNextExpectedChar == word.length() ) {
+						// we found wordNode to be one solution
+						// and we should go next wordNode
+						solutions.addLast( wordNode );
+						indexOfNextExpectedChar = 1;
+						continue;// this will go to while and attempt to go next
+						// wordNode
 					} else {
-						//it's another char, bad, we go back, attempt to get next IN for this char
-						goto HERE;
+						// indexOfNextExpectedChar > word.length()
+						throw new BugError( "went over bounds" );
+					}// else
+				}// else
+			}// else
+		}// while
+		
+		return solutions;// can be empty
+	}
+	
+	/**
+	 * recursive method
+	 * 
+	 * @param wordNode
+	 * @param expectedString
+	 *            the chars in the order in which they're expected to be found
+	 * @param indexOfExpectedChar
+	 *            the index of the next char that is to be expected to exist<br>
+	 *            this index is only applied on <tt>expectedString</tt>
+	 * @param lastINFound
+	 *            we're skipping this, and the next IntermediaryNode found will
+	 *            be either CharNode or WordNode<br>
+	 *            if CharNode then it must be equal to expectedString[charIndex]
+	 *            and if so charIndex++ and go on with parsing; if CharNode is
+	 *            different char than expected then abort with return -1 and you
+	 *            should choose another wordNode<br>
+	 *            else if WordNode, digDeep() until u find a CharNode then apply
+	 *            what we said above for CharNode<br>
+	 * @return the index of the next char that needs to be checked for<br>
+	 *         OR -1 if there was an unexpected char encountered, which means, u
+	 *         should get another <tt>wordNode</tt>
+	 */
+	private int digDeep( NodeWithDupChildren wordNode, NodeList expectedString,
+			int indexOfExpectedChar, IntermediaryNode lastINFound ) {
+
+		Debug.nullException( wordNode, expectedString, indexOfExpectedChar );
+		IntermediaryNode in = lastINFound;// can be null
+		
+		// if more chars to go, and no bad char encountered
+		while ( ( indexOfExpectedChar < ( expectedString.size() - 1 ) )
+				&& ( indexOfExpectedChar >= 0 ) ) {
+			// this is like parallel on the X axis; same wordNode parent
+			in = wordNode.getNextIntermediary( in );
+			if ( null == in ) {
+				// no more intermediaries? then break/exit method
+				// and if word[0] has this wordNode then you should go upward,
+				// on a parent of this wordNode that has this wordNode on
+				// position 0 and check that parent's children for next expected
+				// chars or wordNodes that contain the expected chars
+				break;
+			} else {
+				// found one, whose child may be Word or Char node
+				Node wordOrChar = in.getPointee();
+				if ( Environment.isCharNode( wordOrChar ) ) {
+					// is CharNode then we check if it's the expected char
+					if ( wordOrChar == expectedString.getNodeAt( indexOfExpectedChar ) ) {
+						// good, now expect next char
+						indexOfExpectedChar++;
+						continue;
+					} else {
+						// bad char, the char we found was different than the
+						// expected char
+						// therefore we must choose another wordNode,
+						// ie. next on Z axis
+						indexOfExpectedChar = -1;
+						break;
 					}
 				} else {
-					//it's not charNode
-					if (Environment.isWordNode( whatIs )) {
-						//TODO
-					}else {
-						throw new BugError("should only be WordNode or CharNode");
-					}
-				}
-			} else {// no more?
-				//no more intermediaries for wordNode, but yes we have more word chars
-				//then you need to go up of this wordNode
-				if (charIndex > 0) {
-					//we're not at first char
-					charIndex--;//go prev
-					goto NEXTWORDNODE;
-				}
-			}
-		}
+					// it's not Char
+					if ( Environment.isWordNode( wordOrChar ) ) {
+						indexOfExpectedChar = this.digDeep(
+								(NodeWithDupChildren)wordOrChar,
+								expectedString, indexOfExpectedChar, null );
+						continue;
+					} else {
+						// not char not word?!
+						throw new BugError(
+								"supposed to be CharNode OR WordNode not something else" );
+					}// else
+				}// else
+			}// else
+		}// while
 		
-
-
-		return null;
+		return indexOfExpectedChar;
 	}
 	
 	/**
 	 * we looking for a parent NodeWithDupChildren that has AllWords as parent
-	 * AND also has chrNode as child at the specified indexPos;<br>
+	 * AND also has forNode as child at the specified indexPos;<br>
 	 * AND we're continuing from previouslyFound IntermediaryNode, by skipping
 	 * it
 	 * 
-	 * @param chrNode
+	 * @param forNode
 	 * @param indexPos
 	 *            position of chrNode in the list of children for the returned
 	 *            node
 	 * @param previouslyFoundIN
 	 *            continue from this IntermediaryNode, skipping it; go next;<br>
 	 *            if null, try first IntermediaryNode found while parsing from
-	 *            the beginning of parents of chrNode
+	 *            the beginning of parents of forNode
 	 * @return an IntermediaryNode that is between IntermediaryNode.getFather()
-	 *         which you wanted AND chrNode<br>
+	 *         which you wanted AND forNode<br>
 	 *         you would append .getFather() on the return node to get the
 	 *         WordNode
 	 */
-	protected IntermediaryNode getNextIntermediaryNodeForCharAt(
-			CharNode chrNode, int indexPos, IntermediaryNode previouslyFoundIN ) {
+	protected IntermediaryNode getNextIntermediaryNodeForNodeAt( Node forNode,
+			int indexPos, IntermediaryNode previouslyFoundIN ) {
 
-		Debug.nullException( chrNode, indexPos );
+		Debug.nullException( forNode, indexPos );
 		Node parser = null;
 		if ( null != previouslyFoundIN ) {
 			parser = previouslyFoundIN;
 			if ( !Environment.isIntermediaryNode( parser ) ) {
 				throw new BadParameterException( "wrong parameter" );
 			}
-			if ( ( (IntermediaryNode)parser ).getPointee() != chrNode ) {
+			if ( ( (IntermediaryNode)parser ).getPointee() != forNode ) {
 				throw new BadParameterException(
 						"chrNode is not a child of previouslyFound" );
 			}
@@ -200,7 +276,7 @@ public class WordMapping extends CharMapping {
 		
 
 		// parser can be null here before assignment
-		parser = chrNode.getNextParent( Environment.AllIntermediaryNodes,
+		parser = forNode.getNextParent( Environment.AllIntermediaryNodes,
 				parser );
 		// we're here having the next potential intermediary node, under
 		// 'parser'
@@ -225,7 +301,7 @@ public class WordMapping extends CharMapping {
 			
 			// try next parent, but only look for intermediary nodes, and
 			// continue from last found parent
-			parser = chrNode.getNextParent( Environment.AllIntermediaryNodes,
+			parser = forNode.getNextParent( Environment.AllIntermediaryNodes,
 					parser );
 		}
 		
