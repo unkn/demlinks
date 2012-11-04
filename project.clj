6adb3cd028868542183d27806d197fc0890107a8
AtKaaZ <atkaaz@gmail.com>
2012-11-04 03:20:02 +0100

    adding datomic and midje as deps

--- project.clj
+++ project.clj
@@ -3,4 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-beta1"]])
+  :dependencies [
+                 [org.clojure/clojure "1.5.0-beta1"]
+                 [com.datomic/datomic-free "0.8.3591"]
+                 [midje "1.4.0"]
+                 ]
+  
+  :warn-on-reflection true
+  
+  :plugins [
+            [lein-midje "2.0.0-SNAPSHOT"]
+            ]
+  
+  )
