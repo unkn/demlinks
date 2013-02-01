8eb675eedcc08a54f0606a502c00ea518faf45f6
AtKaaZ <atkaaz@gmail.com>
2013-02-01 17:40:15 +0100

    spend hours on macros, go figure

--- src/runtime/testengine.clj
+++ src/runtime/testengine.clj
@@ -2,13 +2,18 @@
   (:use clojure.test)
   )
 
-(def ^:private a RuntimeException)
-(defn ^:private b [] (throw (RuntimeException. "1"))) 
+(def ^:private a java.lang.RuntimeException)
+(defn ^:private b [] (throw (java.lang.RuntimeException. "1"))) 
 
-(with-test 
-  (defmacro isthrown?
+(defmacro isthrown?
     [cls & restt]
-    (let [tocls (eval cls)]
+    (let [tocls 
+          (eval cls)
+          ;(symbol (str (eval cls)))
+          ;tocls2 (read-string (str "(quote " (eval cls) ")")) 
+          ;_ (prn tocls2)
+          ;(class tocls)
+          ]
       `(is 
          (
            ~'thrown? ;thanks Anderkent for unquote-quote here
@@ -18,11 +23,17 @@
          )
       )
     )
+;XXX: with-test is bad when modifying the defined func/macro and 
+;it fails on reload (ctrl+alt+L) the old version will be used in 
+;tests and never reloaded unless repl restart
+(deftest test_isthrown?
   (is (thrown? java.lang.RuntimeException (b)))
   (isthrown? a (b))
-  (is (=
+  ;FIXME: if ever, the following will fail due to symbol vs class  apparently 
+  ;as detected by Anderkent, exact test case here: https://gist.github.com/4691902
+  (is (not=
         (macroexpand-1 
-          '(isthrown? a (throw (RuntimeException. "1")))
+          '(isthrown? a (throw (java.lang.RuntimeException. "1")))
           )
         '(clojure.test/is (thrown? java.lang.RuntimeException (throw (RuntimeException. "1"))))
         ))
