081e887e8f23ab22e0d6f5bcf05e68b4a17220fd
AtKaaZ <atkaaz@gmail.com>
2013-02-01 16:36:17 +0100

    as-is now, lists-comparison fail https://gist.github.com/4691902

--- src/runtime/testengine.clj
+++ src/runtime/testengine.clj
@@ -2,24 +2,38 @@
   (:use clojure.test)
   )
 
-(defmacro isthrown?
-  [cls & restt]
-  (list 
-    `is 
-    (let [tocls (eval cls)] 
-      `(
-         ~'thrown? ;thanks Anderkent for unquote-quote here
-         ~tocls
-         ~@restt
+(def ^:private a RuntimeException)
+(defn ^:private b [] (throw (RuntimeException. "1"))) 
+
+(with-test 
+  (defmacro isthrown?
+    [cls & restt]
+    (let [tocls (eval cls)]
+      `(is 
+         (
+           ~'thrown? ;thanks Anderkent for unquote-quote here
+           ~tocls
+           ~@restt
+           )
          )
       )
     )
+  (is (thrown? java.lang.RuntimeException (b)))
+  (isthrown? a (b))
+  (is (=
+        (macroexpand-1 
+          '(isthrown? a (throw (RuntimeException. "1")))
+          )
+        '(clojure.test/is (thrown? java.lang.RuntimeException (throw (RuntimeException. "1"))))
+        ))
+  
   )
 
-(def a RuntimeException)
 
-(macroexpand-1 
-  '(isthrown? a (throw (RuntimeException. "1")))
-)
+;(macroexpand-1 
+;  '(isthrown? a (throw (RuntimeException. "1")))
+;)
+;=
+;(clojure.test/is (thrown? java.lang.RuntimeException (throw (RuntimeException. "1"))))
 
-;(isthrown? a (throw (RuntimeException. "1")))
+
