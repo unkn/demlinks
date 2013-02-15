def14cf949aaa6d6bfe9703a9f0376703dfb5038
AtKaaZ <atkaaz@gmail.com>
2013-02-15 04:50:25 +0100

    in-ns good but ccw doesn't recognize it as a namespace so fail

--- src/random/learning/clojure/usetheoverridexns.clj
+++ src/random/learning/clojure/usetheoverridexns.clj
@@ -1,26 +1,28 @@
-(ns random.learning.clojure.usetheoverridexns
-  (:refer-clojure :exclude [sorted?])
-  (:use [random.learning.clojure.overridex])
-  (:refer-clojure :exclude [sorted?])
-  ;(:require [random.learning.clojure.overridex])
-  )
+;not bad, but ccw functionality lost: switching to this namespace(on load and on demand)
+(clojure.core/in-ns 'random.learning.clojure.usetheoverridexns)
+(clojure.core/require '[clojure.core :as cc])
 
-(println clojure.core/*ns*)
+;  (:refer-clojure :exclude [sorted?])
+  (cc/use '[random.learning.clojure.overridex])
+  ;(:refer-clojure :exclude [sorted?])
+  ;(:require [random.learning.clojure.overridex])
+  ;)
+
+(cc/println clojure.core/*ns*)
 ;the following only works if pasted in REPL, cause ccw seems to be loading clojure.core after executing this file
-(clojure.core/map 
+#_(clojure.core/map 
   #(clojure.core/ns-unmap clojure.core/*ns* %)
   (clojure.core/keys 
     (clojure.core/ns-publics 'clojure.core)
     )
   )
 
-(require '[clojure.core :as cc])
 ;require
 ;(refer '[clojure.core :as cc])
 
-(and 
-  (= false (sorted? '(1 2)))
-  (= true (sorted? (sorted-set 1 2))))
+(cc/and 
+  (cc/= false (sorted? '(1 2)))
+  (cc/= true (sorted? (cc/sorted-set 1 2))))
 
 ;;
 ;; Clojure 1.5.0-RC16
