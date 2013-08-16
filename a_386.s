5f1832d8837f61b2c6ee463ee9b7b91fc8a40244
AtKaaZ <atkaaz@gmail.com>
2013-08-16 16:08:43 +0200

    as suggested by Jonathan Barnard, go will pick the right one

---
+++ a_386.s
@@ -0,0 +1,19 @@
+//TODO: unsure if any changes are required here for the x86 version, this is what works on x64
+
+//this code is from here: https://groups.google.com/d/msg/golang-nuts/q01oIascmiU/wuoqrF_TOqgJ
+//thanks to Jonathan Barnard
+
+TEXT ·FlipByte(SB),7,$0
+		MOVQ	addr+0(FP), BP // This gets the address of the byte pointed to. 
+    MOVL	0(BP), DX // This stores the byte in DX 
+    MOVL	$8, CX // Use the CX register as a loop counter 
+    XORL	AX, AX // Clear the AX register, to store the result in
+
+loop: // This is a label to be jumped to.
+		SHLB	$1, DX // Shift the byte in DX right, pushing the leftmost bit into the carry flag 
+		RCRB	$1, AX // Rotate with carry the value in AX to the left, pushing the bit in the carry flag to the leftmost position in AX. 
+    DECB  CX // Decrement the counter by one
+    JNZ	loop // Jump to the loop label if the counter isn't zero
+
+MOVL AX, 0(BP) //Move the result back into the address of the byte pointed to (modify the value pointed to by the pointer passed into the function).
+RET //Return from the function
--- a_amd64.s
+++ a_amd64.s
@@ -1,5 +1,6 @@
 
 //this code is from here: https://groups.google.com/d/msg/golang-nuts/q01oIascmiU/wuoqrF_TOqgJ
+//thanks to Jonathan Barnard
 
 TEXT ·FlipByte(SB),7,$0
     MOVQ	addr+0(FP), BP // This gets the address of the byte pointed to. 
@@ -8,10 +9,10 @@ TEXT ·FlipByte(SB),7,$0
     XORL	AX, AX // Clear the AX register, to store the result in
 
 loop: // This is a label to be jumped to.
-     SHLB	$1, DX // Shift the byte in DX right, pushing the leftmost bit into the carry flag 
-		 RCRB	$1, AX // Rotate with carry the value in AX to the left, pushing the bit in the carry flag to the leftmost position in AX. 
-     DECB  CX // Decrement the counter by one
-     JNZ	loop // Jump to the loop label if the counter isn't zero
+    SHLB	$1, DX // Shift the byte in DX right, pushing the leftmost bit into the carry flag 
+		RCRB	$1, AX // Rotate with carry the value in AX to the left, pushing the bit in the carry flag to the leftmost position in AX. 
+    DECB  CX // Decrement the counter by one
+    JNZ	loop // Jump to the loop label if the counter isn't zero
 
 MOVL AX, 0(BP) //Move the result back into the address of the byte pointed to (modify the value pointed to by the pointer passed into the function).
 RET //Return from the function
