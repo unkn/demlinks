fc3db383a6de0bfd3a7b66ed9b4256c4206389c1
AtKaaZ <atkaaz@gmail.com>
2013-08-19 16:40:11 +0200

    failed attempt to test/use pprof, we'll leave that for later

--- poc/race/race.go
+++ poc/race/race.go
@@ -1,4 +1,3 @@
-
 //this code is from here: https://groups.google.com/d/msg/golang-nuts/NDQBTV9lH7s/gdEn7iqjpDUJ
 //demonstrates a race condition, to run: go run -race race.go
 //you can also do: go build
@@ -8,12 +7,16 @@ package main
 
 import (
 	"fmt"
-	"time"
-
 	"math/rand"
+//	_ "net/http/pprof"
+	"time"
 )
 
 func main() {
+/*	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()*/
+
 	x := fanIn(boring("A"), boring("B"), boring("C"), boring("D"))
 	for i := 0; i < 20; i++ {
 		fmt.Println(<-x)
@@ -38,7 +41,7 @@ func boring(msg string) <-chan string {
 	go func() { // We launch the goroutine from inside the function.
 		for i := 0; ; i++ {
 			c <- fmt.Sprintf("%s %d", msg, i)
-			time.Sleep(time.Duration(rand.Intn(1e3))*time.Millisecond)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 		}
 	}()
 	return c // Return the channel to the caller.
