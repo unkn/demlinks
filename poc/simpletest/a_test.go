b939178c3b7db8106b08ee4f1ca3ef517f691e32
AtKaaZ <atkaaz@gmail.com>
2013-08-27 23:15:55 +0200

    tracking down this error when build folder isn't deleted

---
+++ poc/simpletest/a_test.go
@@ -0,0 +1,24 @@
+package demlinks
+
+import (
+	// "fmt"
+	// "os"
+	// "os/exec"
+	// "path/filepath"
+	"testing"
+	// "time"
+)
+
+func TestNothing(t *testing.T) {
+	/*
+		temp := os.Getenv("TEMP")
+		what := filepath.Join(temp, "a.exe")
+		c := exec.Command(what)
+		c.Run()
+		time.Sleep(1000 * time.Millisecond)
+
+		//os.OpenFile(what, flag, perm)
+
+		fmt.Println(os.Remove(what))*/
+	t.FailNow()
+}
