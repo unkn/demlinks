79a14d5b58a9f256d6149b53649d0af96b8d8da1
AtKaaZ <atkaaz@gmail.com>
2013-08-27 17:33:42 +0200

    ok made test to fail on first error rather than cumulate some error before failing

--- poc/lookpathtest/lp_windows_test.go
+++ poc/lookpathtest/lp_windows_test.go
@@ -24,7 +24,7 @@ func TestLookPath1(t *testing.T) {
 
 	tmp, err1 := ioutil.TempDir("", "testWindowsLookPathTests")
 	if err1 != nil {
-		myt.Fatalf("TempDir failed: %v", err1)
+		myt.Quitf("TempDir failed: %v", err1)
 	}
 	//TODO: defer os.RemoveAll(tmp)
 
@@ -34,13 +34,13 @@ func TestLookPath1(t *testing.T) {
 	// myt.setEnv("COMSpec", "cmd.exe")
 	comspec := os.Getenv(comspecvar)
 	if comspec == "" {
-		myt.Fatalf("%s must be set", comspecvar)
+		myt.Quitf("%s must be set", comspecvar)
 	}
 
 	dir, file := filepath.Split(comspec)
 	// println(file)
 	if dir == "" {
-		myt.Fatalf("Expected to have a path in %%%s%% 's value: `%s`", comspecvar, comspec)
+		myt.Quitf("Expected to have a path in %%%s%% 's value: `%s`", comspecvar, comspec)
 	}
 
 	//TODO: paths in PATH with & without suffix of "\\"
@@ -53,15 +53,13 @@ func TestLookPath1(t *testing.T) {
 		"",
 	}
 
-	//TODO: should the entire test fail ie. FailNow() when one of them fails? hmm, maybe it's best that way
+	//myt.ensureExitsAsFile(tmp)
+	//done: should the entire test fail ie. FailNow() when one of them fails? hmm, maybe it's best that way
 	//because otherwise there will be too many errors reported anyway
 
-	if err := myt.testFor(file, dir+file, paths); err != nil {
-		myt.Error(err)
-	} else {
-		balddir := strings.TrimSuffix(dir, folders_separator)
-		myt.testFor(file, balddir+file, paths)
-	}
+	myt.testFor(file, dir+file, paths)
+	balddir := strings.TrimSuffix(dir, folders_separator)
+	myt.testFor(file, balddir+file, paths)
 
 	//C:\folder1\folder2\file
 	//C:\folder1\folder2\file.
@@ -70,13 +68,17 @@ func TestLookPath1(t *testing.T) {
 	//C:\folder1\folder2\file.multi.dot.extension
 	//C:\folder1\folder2\file.multi.dot.ext
 	//C:\folder1\folder2\file.multi.dot.
-	//replace dot with from "" until "dotext"
+	//replace dot with from "" until "dotext", so "", "d","do","dot","dote","dotex","dotext"
 	//replace ext with from "" until "extension"
 	//replace file with from "" until "fileover8chars"
 	//also use spaces for each step in the above ie. for dot(above^) ""," ", "d", "d "," d"," d ", "do", " do","do ", " do " etc...
 	//if testing.Short() then don't iterate that many variants
 	//replace folder1 or 2 with same concept as for ext/dot/file
 	//also do uppercase variants, maybe just all upper, all lower, and one char upper and rest lower (and reverse of this)
+	//PATH should contain some inexisting folders to search into which are before and after the correct folder
+	//sometimes PATH should contain only the correct folder(cf), sometimes only normal but not cf folder
+	// and inexsiting folders and such combinations
+	//and add to that each path element in pathlist would end or not with "\\"
 
 	//TODO: try to make a bunch of 0 bytes files in tmp folder that we got, and dirs + symlinks / combinations
 	//and then do try those as if they were to be found by LookPath, obvious set the PATH to point to tmp folder
@@ -86,13 +88,14 @@ func TestLookPath1(t *testing.T) {
 
 	f, err := ioutil.TempFile(tmp, "prefix")
 	if err != nil {
-		myt.Fatalf("unable to create a temp file in temp folder `%s`, error: `%s`", tmp, err)
+		myt.Quitf("unable to create a temp file in temp folder `%s`, error: `%s`", tmp, err)
 	}
 	println(f.Name())
 	//filepath.Join(tmp,f.Name())
 	//os.Symlink(tmp+, newname)
 
 	//TODO: must check if os.Symlink is really not implemented on Windows, if it's not then try to implement it prior to this
+
 }
 
 //adding extra "methods" to testing.T to avoid having to pass it as an arg
@@ -109,29 +112,26 @@ type TMine testing.T
 	}
 }*/
 
-func (t *TMine) MyErrorf(format string, args ...interface{}) []interface{} {
-	t.Errorf(format, args...)
-	return args
+func (t *TMine) Quit(args ...interface{}) {
+	t.Fatal(args...)
 }
 
-func (t *TMine) testFor(lookfor, expected string, paths []string) []interface{} {
+func (t *TMine) Quitf(format string, args ...interface{}) {
+	t.Fatalf(format, args...)
+}
+
+func (t *TMine) testFor(lookfor, expected string, paths []string) {
 	accval := ""
 	for _, val := range paths {
-		if err := t.testWithPATH(val, lookfor, expected); err != nil {
-			return err
-		}
+		t.testWithPATH(val, lookfor, expected)
 		if !testing.Short() {
 			accval += PATH_separator + val
-			if err := t.testWithPATH(accval, lookfor, expected); err != nil {
-				return err
-			}
+			t.testWithPATH(accval, lookfor, expected)
 		}
 	}
-
-	return nil
 }
 
-func (t *TMine) testWithPATH(path, lookfor, expected string) []interface{} {
+func (t *TMine) testWithPATH(path, lookfor, expected string) {
 	t.setEnv(PATHvar, path)
 
 	pathexts := []string{
@@ -143,61 +143,55 @@ func (t *TMine) testWithPATH(path, lookfor, expected string) []interface{} {
 
 	accval := ""
 	for _, val := range pathexts {
-		if err := t.testWithPATHEXT(val, lookfor, expected); err != nil {
-			return err
-		}
+		t.testWithPATHEXT(val, lookfor, expected)
 		if !testing.Short() {
 			accval += PATHEXT_separator + val
-			if err := t.testWithPATHEXT(accval, lookfor, expected); err != nil {
-				return err
-			}
+			t.testWithPATHEXT(accval, lookfor, expected)
 		}
 	}
-
-	return nil
 }
 
-func (t *TMine) testWithPATHEXT(pathext, lookfor, expected string) []interface{} {
+func (t *TMine) testWithPATHEXT(pathext, lookfor, expected string) {
 	t.setEnv(PATHEXTvar, pathext)
-	return t.expectFind(lookfor, expected)
+	t.expectFind(lookfor, expected)
 }
 
-func (t *TMine) expectFind(lookfor, expected string) []interface{} {
+func (t *TMine) expectFind(lookfor, expected string) {
 	found, err := exec.LookPath(lookfor)
 	if err != nil {
-		return t.MyErrorf("Failed to find `%s`, expected to find it as `%s` but got an error instead: `%s`", lookfor, expected, err)
+		t.Quitf("Failed to find `%s`, expected to find it as `%s` but got an error instead: `%s`", lookfor, expected, err)
 	}
 
 	found = strings.ToUpper(found)
 	expected = strings.ToUpper(expected)
 	if found != expected { // && ( != strings.ToUpper(expected)) {
-		return t.MyErrorf("Failed to find `%s`, expected to find it as `%s` but got `%s` instead.", lookfor, expected, found)
+		t.Quitf("Failed to find `%s`, expected to find it as `%s` but got `%s` instead.", lookfor, expected, found)
 	}
 
-	return t.ensureExitsAsFile(found)
+	t.ensureExitsAsFile(found)
 }
 
 //as file or symlink to file, even if symlink is broken
-func (t *TMine) ensureExitsAsFile(fname string) []interface{} {
+func (t *TMine) ensureExitsAsFile(fname string) {
 	finfo, err := os.Stat(fname)
 	if err != nil {
-		return t.MyErrorf("Expected `%s` to exist, error: `%s`", fname, err)
+		t.Quitf("Expected `%s` to exist, error: `%s`", fname, err)
 	}
 	if !finfo.IsDir() {
 		//could be symlink file
 		//TODO: test if searched file is symlinkd or symlinkfile or dir or normal file, also readonly file, sysfile, hidden file + dirs
 		//also when symlinkfile is broken (points to nothing)
-		return nil
+		return
 	}
-	return t.MyErrorf("Expected `%s` to exist as a file, but it's got unexpected mode `%s`", fname, finfo.Mode())
+	t.Quitf("Expected `%s` to exist as a file, but it's a directory with this mode `%s`", fname, finfo.Mode())
 }
 
 func (t *TMine) setEnv(key, value string) {
 	if err := os.Setenv(key, value); err != nil {
-		t.Fatal(err)
+		t.Quit(err)
 	}
 
 	if now_val := os.Getenv(key); value != now_val {
-		t.Fatalf("Couldn't set `%s` env var, \ncurrent value: \n`%s`, \nwanted value: \n`%s`", key, now_val, value)
+		t.Quitf("Couldn't set `%s` env var, \ncurrent value: \n`%s`, \nwanted value: \n`%s`", key, now_val, value)
 	}
 }
