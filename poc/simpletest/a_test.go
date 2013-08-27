501b227a8a22dfe0c0b2855073a16b5df0ab52d9
AtKaaZ <atkaaz@gmail.com>
2013-08-28 00:29:02 +0200

    now it's reproducible

--- poc/simpletest/a_test.go
+++ poc/simpletest/a_test.go
@@ -6,27 +6,30 @@ import (
 	"os/exec"
 	"path/filepath"
 	"testing"
-	// "time"
+	"time"
 )
 
 func TestNothing(t *testing.T) {
+	//t.FailNow() //XXX: uncomment this when executing `go test -work` for the first time
+	//XXX: then rename the gobuild#### folder to `test1` and make a backup of it somewhere so you can get it back quick in this same place
+	//the comment this and execute `go test`
+	//notice the test1 folder remained with the .exe inside
+	//run `go test` again and it's gone
+	//uncomment the time.Sleep below to make it always work
+	//it's a matter of time... unsure if windows is delaying the .exe unlocking after execute or what
+	// I do remember it used to keep them locked for 60 seconds some time ago, not sure why but I fixed that once
+	// the point is though, that's possible that windows is keeping it locked a few ms more even after it finished execution
+	// (PS: i've no antivirus or defense+ programs to keep it locked, maybe virus then? or windows)
 
-	//temp := os.Getenv("TEMP")
-
-	// tmp, err1 := ioutil.TempDir("", "test1")
-	// if err1 != nil {
-	// 	t.Fatalf("TempDir failed: %v", err1)
-	// 	// } else {
-	// 	// 	defer func() {
-	// 	// 		fmt.Println(os.RemoveAll(tmp))
-	// 	// 	}()
-	// }
 	temp := filepath.Join(os.Getenv("TEMP"), "test1")
 	os.Mkdir(temp, os.ModePerm)
-	defer func() { fmt.Println("testerrrr:", os.RemoveAll(temp)) }()
+	defer func() {
+		//time.Sleep(2000 * time.Millisecond) //XXX: uncomment this to have delete always work!
+		fmt.Println("testerrrr:", os.RemoveAll(temp))
+		_ = time.ANSIC //avoid import "time" unused error
+	}()
 
-	what := filepath.Join(temp, "a.exe")
-	//fmt.Println(temp, what)
+	what := filepath.Join(temp, "github.com", "demlinks", "demlinks", "poc", "simpletest", "_test", "simpletest.test.exe")
 
 	c := exec.Command(what)
 	c.Stdout = os.Stdout
@@ -41,18 +44,10 @@ func TestNothing(t *testing.T) {
 			done <- c.Wait()
 			fmt.Println("after wait2!")
 		}()
-		//fmt.Println("waited:", c.Wait())
 		select {
 		case err = <-done:
 			// ok
 		}
 		fmt.Println("after else:", err)
 	}
-	//time.Sleep(1000 * time.Millisecond)
-
-	//os.OpenFile(what, flag, perm)
-
-	//filepath.Join(tmp,what)
-
-	//t.FailNow()
 }
