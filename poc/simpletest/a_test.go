c4fbdedfd3290025e968b53ea24c256f8aaf5ea8
AtKaaZ <atkaaz@gmail.com>
2013-08-28 00:13:56 +0200

    this just doesn't wanna reproduce, so far

--- poc/simpletest/a_test.go
+++ poc/simpletest/a_test.go
@@ -1,24 +1,58 @@
 package demlinks
 
 import (
-	// "fmt"
-	// "os"
-	// "os/exec"
-	// "path/filepath"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 	// "time"
 )
 
 func TestNothing(t *testing.T) {
-	/*
-		temp := os.Getenv("TEMP")
-		what := filepath.Join(temp, "a.exe")
-		c := exec.Command(what)
-		c.Run()
-		time.Sleep(1000 * time.Millisecond)
 
-		//os.OpenFile(what, flag, perm)
+	//temp := os.Getenv("TEMP")
 
-		fmt.Println(os.Remove(what))*/
-	t.FailNow()
+	// tmp, err1 := ioutil.TempDir("", "test1")
+	// if err1 != nil {
+	// 	t.Fatalf("TempDir failed: %v", err1)
+	// 	// } else {
+	// 	// 	defer func() {
+	// 	// 		fmt.Println(os.RemoveAll(tmp))
+	// 	// 	}()
+	// }
+	temp := filepath.Join(os.Getenv("TEMP"), "test1")
+	os.Mkdir(temp, os.ModePerm)
+	defer func() { fmt.Println("testerrrr:", os.RemoveAll(temp)) }()
+
+	what := filepath.Join(temp, "a.exe")
+	//fmt.Println(temp, what)
+
+	c := exec.Command(what)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	//c.Run()
+	err := c.Start()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		done := make(chan error)
+		go func() {
+			done <- c.Wait()
+			fmt.Println("after wait2!")
+		}()
+		//fmt.Println("waited:", c.Wait())
+		select {
+		case err = <-done:
+			// ok
+		}
+		fmt.Println("after else:", err)
+	}
+	//time.Sleep(1000 * time.Millisecond)
+
+	//os.OpenFile(what, flag, perm)
+
+	//filepath.Join(tmp,what)
+
+	//t.FailNow()
 }
